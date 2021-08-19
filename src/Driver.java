ea179d0abe0ac5d5889717635222fc4f8e90dfed
ElifnurBenhur <elifnur.benhur@gmail.com>
2021-08-20 01:32:33 +0300

    second commit

--- src/Driver.java
+++ src/Driver.java
@@ -2,5 +2,6 @@ public class Driver {
     public static void main(){
         System.out.println("Versiyon 1");
         System.out.println("Bilgisayardan ekleme");
+        System.out.println("Bilgisayardan ekleme2");
     }
 }
